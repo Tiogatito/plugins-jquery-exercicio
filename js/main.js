9667e880209ac434d3f847ec24797e115b55caba
Mateus <mateusmateusc7@gmail.com>
2026-09-06 15:19:21 -0300

    feat: implementa carrossel slick, mascaras e validacao

--- js/main.js
+++ js/main.js
@@ -1,42 +1,63 @@
-$('#telefone').mascara('(00) 00000-0000', {
+$(document).ready(function () {
+  // 1. Carrossel Slick
+  $('#carousel-imagens').slick({
+    autoplay: true,
+    arrows: false
+  });
+
+  // 2. Máscaras dos campos
+  $('#telefone').mask('(00) 00000-0000', {
     placeholder: '(DDD) 12345-6789'
-})
+  });
 
-$('#cpf').mascara('000.000.000-00', {
+  $('#cpf').mask('000.000.000-00', {
     placeholder: '123.456.789-00'
-})
+  });
 
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
+  $('#cep').mask('00000-000', {
+    placeholder: '01234-567'
+  });
 
-$('form').validate({
+  // 3. Validação do formulário
+  $('#form').validate({
     rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
+      nome: {
+        required: true
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      telefone: {
+        required: true
+      },
+      cpf: {
+        required: true
+      },
+      endereco: {
+        required: true
+      },
+      cep: {
+        required: true
+      }
+    },
+    messages: {
+      nome: 'Por favor, insira o seu nome completo',
+      email: 'Por favor, insira um e-mail válido',
+      telefone: 'Por favor, insira o seu telefone celular',
+      cpf: 'Por favor, insira o seu CPF',
+      endereco: 'Por favor, insira o seu endereço completo',
+      cep: 'Por favor, insira o seu CEP'
     },
     submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
+      alert('Formulário enviado com sucesso!');
+      form.reset();
     },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
+    invalidHandler: function (evento, validador) {
+      const erros = validador.numberOfInvalids();
+      if (erros) {
+        alert(`Por favor, preencha os ${erros} campos pendentes antes de enviar.`);
+      }
     }
-})
+  });
+});
